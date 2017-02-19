b0daa9e182b2c2a9147d5c8f32a0ee2fe10f7338
Slowhand0309 <slowhand0309@gmail.com>
2017-02-19 09:08:46 +0900

    Add firebase

--- app/src/main/java/com/app/slowhand/settingssample/MainActivity.java
+++ app/src/main/java/com/app/slowhand/settingssample/MainActivity.java
@@ -5,11 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.app.slowhand.settingssample.databinding.ActivityMainBinding;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
 
     private Settings mSettings;
     private ActivityMainBinding mBinding;
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +21,6 @@ public class MainActivity extends AppCompatActivity {
         mSettings = new Settings(this);
         mBinding.setSettings(mSettings);
 
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
     }
 }
